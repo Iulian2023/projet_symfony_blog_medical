0ce502a171c77947347935d5178f6705ab550afb
Iulian <rotaruiulian19@gmail.com>
2023-08-08 16:40:32 +0200

    Mise en place de vue, creation et de supprimer un article

---
+++ src/Form/PostFormType.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Post;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class PostFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('category', EntityType::class, [
+                // looks for choices from this entity
+                'class' => Category::class,
+            
+                'placeholder' => 'Choisissez un catégorie',
+                // uses the Category.name property as the visible option string
+                'choice_label' => 'name',
+            ])
+            ->add('imageFile', VichImageType::class)
+            ->add('imageFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'Supprimer l\'image',
+                'download_label' => false,
+                'download_uri' => false,
+                'image_uri' => false,
+                'imagine_pattern' => false,
+                'asset_helper' => true,
+            ])
+            ->add('content', TextareaType::class)
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+        ]);
+    }
+}
